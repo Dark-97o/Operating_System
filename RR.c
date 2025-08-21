3b13c912eccd4f1208608e9ae5f1241ee0639ab2
Subhranil Baul <114503763+Dark-97o@users.noreply.github.com>
2025-08-21 11:14:50 +0530

    commit

--- RR.c
+++ RR.c
@@ -2,6 +2,7 @@
 
 int main() {
     int n, tq;
+    float tat_avg = 0, wt_avg = 0;
     printf("Enter number of processes: ");
     scanf("%d", &n);
 
@@ -30,8 +31,10 @@ int main() {
                     if (wt[i] < 0) wt[i] = 0;
                     rt[i] = 0;
                     complete++;
+                    wt_avg = wt_avg +  wt[i];
                 }
             }
+            
         }
         if (done) time++;
     }
@@ -39,11 +42,13 @@ int main() {
     printf("\nP\tAT\tBT\tCT\tTAT\tWT\n");
     for (int i=0; i<n; i++) {
         tat[i] = ct[i] - at[i];
+        tat_avg = tat_avg + tat[i];
         printf("P%d\t%d\t%d\t%d\t%d\t%d\n", i+1, at[i], bt[i], ct[i], tat[i], wt[i]);
     }
-    int avg_tat = avg_tat/n;
-    int avg_wt = avg_wt/n;
-    printf("\nAverage Turnaround Time = %.2f", avg_tat);
-    printf("\nAverage Waiting Time    = %.2f\n", avg_wt);
+    printf("\nAverage Turnaround Time = %.2f", tat_avg/n);
+    printf("\nAverage Waiting Time    = %.2f\n", wt_avg/n);
 
 }
+
+
+
