bc9e2506bfc5be910f5adae5295adafdcaceb1e0
Subhranil Baul <114503763+Dark-97o@users.noreply.github.com>
2025-08-22 14:38:15 +0530

    commit

--- SRTF.c
+++ SRTF.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include <limits.h>
 
 int main() {
     int n;
@@ -8,7 +7,7 @@ int main() {
     scanf("%d", &n);
 
     int at[n], bt[n], rt[n], ct[n], wt[n], tat[n];
-    for(int i=0; i<n; i++) {
+    for (int i=0; i<n; i++) {
         printf("Arrival time of P%d: ", i+1);
         scanf("%d", &at[i]);
         printf("Burst time of P%d: ", i+1);
@@ -16,41 +15,37 @@ int main() {
         rt[i] = bt[i];
     }
 
-    int complete = 0, time = 0, shortest, minm = INT_MAX, finish_time;
-    int check = 0;
-    while (complete != n) {
+    int complete = 0, time = 0;
+    while (complete < n) {
+        int shortest = -1, minm = 99999;
         for (int j=0; j<n; j++) {
-            if ((at[j] <= time) && (rt[j] < minm) && rt[j] > 0) {
+            if (at[j] <= time && rt[j] > 0 && rt[j] < minm) {
                 minm = rt[j];
                 shortest = j;
-                check = 1;
             }
         }
+        if (shortest == -1) {
+            time++;
+            continue;
+        }
+        rt[shortest]--;
 
-        if (check == 0) { time++; continue; }
-        rt[shortest]--; // run process 1 unit
-        minm = rt[shortest];
-        if (minm == 0) minm = INT_MAX;
-
-        if (rt[shortest] == 0) {
+        if (rt[shortest] == 0) { 
             complete++;
-            check = 0;
-            finish_time = time + 1;
-            ct[shortest] = finish_time;
-            wt[shortest] = finish_time - bt[shortest] - at[shortest];
+            ct[shortest] = time + 1;
+            wt[shortest] = ct[shortest] - bt[shortest] - at[shortest];
             if (wt[shortest] < 0) wt[shortest] = 0;
-            
         }
         time++;
     }
-
     printf("\nP\tAT\tBT\tCT\tTAT\tWT\n");
     for (int i=0; i<n; i++) {
         tat[i] = ct[i] - at[i];
-        avg_tat = avg_tat + tat[i];
+        avg_tat += tat[i];
         avg_wt += wt[i];
         printf("P%d\t%d\t%d\t%d\t%d\t%d\n", i+1, at[i], bt[i], ct[i], tat[i], wt[i]);
     }
     printf("\nAverage Turnaround Time = %.2f", avg_tat/n);
     printf("\nAverage Waiting Time    = %.2f\n", avg_wt/n);
+    return 0;
 }
